a391b1cee0feee00e9eca63dbc68324875287f7e
Makenley Luctamar <luctamak001@warren.k12.in.us>
2024-11-06 08:46:34 -0500

    int

---
+++ main.py
@@ -0,0 +1,28 @@
+#SECTION #1
+color_list = ["red", "green", "blue", "yellow"]
+#TODO: use notes to print first element
+print(color_list[0])
+#TODO: use notes to print last element
+print(color_list[-1])
+
+#SECTION #2
+#TODO: create num_list
+num_list = [4, 6, 4, 2, 9, 4, 1]
+#TODO: use append to add 5 to num_list and print
+num_list.append(5)
+print(num_list)
+
+# Remove an item
+num_list.remove(4)
+print(num_list)
+
+# Count occurrences of '4'
+print(num_list.count(4))
+
+# Find the index of '8'
+print(num_list.index(9))
+
+#SECTION #3
+#TODO: first part of section 3
+#TODO: use index operator [] to alter index 1 to orange.  Use notes
+fruit_list.pop()
